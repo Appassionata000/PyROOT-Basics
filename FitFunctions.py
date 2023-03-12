e50df3f98da4f5a8dd7b71216f13c8d2717f2865
MITSUKI <zhiyu.liu-3@student.manchester.ac.uk>
2023-03-12 11:30:34 +0000

    detele unused functions

--- FitFunctions.py
+++ FitFunctions.py
@@ -57,55 +57,6 @@ def sextic_bkg(x, p):
         
     return p[0] + p[1] * x[0] + p[2] * x[0]**2 + p[3] * x[0]**3 + p[4] * x[0]**4 + p[5] * x[0]**5 + p[6] * x[0]**6
 
-def sig_bkg(x):
-    
-    s = (x - 125e3) / 1.19e3
-    a1, n1, a2, n2 = 1.64, 19.19, 1.70, 38.92
-    
-    A1 = math.pow((n1 / abs(a1)), n1) * math.exp(-a1**2 / 2)
-    A2 = math.pow((n2 / abs(a2)), n2) * math.exp(-a2**2 / 2)
-    B1 = math.pow((n1 / abs(a1) - abs(a1) - s), -n1)
-    B2 = math.pow((n2 / abs(a2) - abs(a2) + s), -n2)
-
-    C = -13581.241 + 1.028 * x + -1.556e-05 * x**2 + 9.054e-11 * x**3 + -1.861e-16 * x**4
-    
-    Const = 0.33 * 408.74
-
-    if s < -a1:
-        result = Const * A1 * B1 + C
-    elif s > a2:
-        result = Const * A2 * B2 + C
-    else:
-        result = Const * math.exp(-s**2 / 2) + C
-
-    return result
-
-def bernstein1(x, p):
-    
-    if (x[0] >= 120e3 and x[0] <= 130e3):
-
-        r.TF1.RejectPoint(True)
-    
-    y = (x[0] - 105e3) / (160e3 - 105e3)
-    
-    return p[0] * y + (1 - y)
-
-def bernstein2(x, p):
-    
-    y = (x[0] - 105e3) / (160e3 - 105e3)
-    
-    return (1 - y)**2 + p[0] * 2 * y * (1 - y) + p[1] * y**2
-
-def bernstein3(x, p):
-    
-    if (x[0] >= 120e3 and x[0] <= 130e3):
-
-        r.TF1.RejectPoint(True)
-    
-    y = (x[0] - 105e3) / (160e3 - 105e3)
-    
-    return (1 - y)**3 + p[0] * 3 * y * (1 - y)**2 + p[1] * 3 * y**2 * (1 - y) + p[2] * y**3
-
 def expPoly1_bkg(x, p):
     
     if (x[0] >= 120e3 and x[0] <= 130e3):
@@ -137,10 +88,6 @@ def expPoly4_bkg(x, p):
         r.TF1.RejectPoint(True)
         
     return p[0] * math.exp(-p[1] * x[0] - p[2] * x[0]**2 - p[3] * x[0]**3 - p[4] * x[0]**4)
-    
-def quartic_tot(x, p):
-    
-    return p[0] * x[0]**4 + p[1] * x[0]**3 + p[2] * x[1]**2 + p[3] * x[0] + p[4]
 
 def gaus_1(x, p):
     
